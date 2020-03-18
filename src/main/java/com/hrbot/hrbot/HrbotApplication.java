bd007b2d5dad72a42fb98917ad3d25170924ea4c
usama fouad <usamaeltmsah@yahoo.com>
2020-03-18 13:12:37 +0200

    Adding evalAnswer() method which get the the candidate's answer and evaluate it

--- src/main/java/com/hrbot/hrbot/HrbotApplication.java
+++ src/main/java/com/hrbot/hrbot/HrbotApplication.java
@@ -2,12 +2,28 @@ package com.hrbot.hrbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class HrbotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrbotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrbotApplication.class, args);
+    }
+
+    @RequestMapping("/send_answer")
+    public double evalAnswer(String answer) {
+        double score = 0.0;
+
+        /*
+         *
+         * ToDo: Receive score from evalModel, and save it in score variable
+         *
+         * */
+
+        return score;
+    }
 
 }
