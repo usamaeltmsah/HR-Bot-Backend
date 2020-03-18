f12a0423756cf4c4d2cb5a0a545852ec8eb56d0f
usama fouad <usamaeltmsah@yahoo.com>
2020-03-18 13:44:56 +0200

    Adding save

--- src/main/java/com/hrbot/hrbot/HrbotApplication.java
+++ src/main/java/com/hrbot/hrbot/HrbotApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 @RestController
 @SpringBootApplication
 public class HrbotApplication {
@@ -25,6 +29,26 @@ public class HrbotApplication {
         * */
 
         return score;
+    }
+
+    public boolean saveScoreToDB(double score, Connection conn, int cand_id) throws SQLException {
+        boolean saved = false;
+        PreparedStatement query = conn.prepareStatement("UPDATE 'candidate' SET 'score' = "+ score + " WHERE 'candidat_id' = + cand_id + ");
+        int count = query.executeUpdate();
+        /*
+         * ToDo: Create the database and check the UPDATE query
+         * */
+
+        // If update done set saved to true
+        if(count > 0)
+        {
+            saved = true;
+        }
+        else
+        {
+            System.err.println("Error in Database !!");
+        }
+        return saved;
     }
 
 }
