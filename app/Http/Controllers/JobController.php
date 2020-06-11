9c7ad259fb4051dcad7d3d79da54444dea2ac676
Muhammed Eldeeb <meldeeb1998@gmail.com>
2020-06-12 00:36:44 +0200

    store new job with validation

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,27 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ApiHelper;
 use App\Job;
+use Validator;
 use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
     public function index(){
-        $rows = Job::get();
-        return $rows;
+        $jobs = Job::orderBy('id', 'desc')->get();
+        $res = ApiHelper::createApiResponse(false, 200, 'there are no jobs', $jobs);
+        return response()->json($res, 200);
     }
 
-    public function show($id){
-        $row = Job::findOrFail($id);
-        return $row;
+    public function show(Job $job)
+    {
+        $res = ApiHelper::createApiResponse(false, 200, 'there are no jobs', $job);
+        return response()->json($res, 200);
     }
 
-    public function store($id, Request $request){
-        dd('store');
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title'                     => ['required', 'string'],
+            'desc'                      => ['required', 'string'],
+            'accept_interviews_from'    => ['required', 'date_format:Y-m-d H:i:s'],
+            'accept_interviews_until'   => ['required', 'date_format:Y-m-d H:i:s'],
+            'interviews_duration'       => ['required', 'date_format:Y-m-d H:i:s'],
+            'recruiter_id'              => ['required', 'integer']
+        ]);
+
+
+        if($validator->fails()) {
+            $res = ApiHelper::createApiResponse(true, 400, $validator->errors()->first(), null);
+            return response()->json($res, 400);
+        }else{
+            try{
+                Job::create($request->all());
+                $res = ApiHelper::createApiResponse(false, 201, 'Job Added Successfully', null);
+                return response()->json($res, 201);
+            }catch (Exception $exc){
+                $res = ApiHelper::createApiResponse(true, 400, $exc, null);
+                return response()->json($res, 400);
+            }
+
+        }
     }
 
-    public function update($id, Request $request){
-        dd('update');
+    public function update(Job $job, Request $request){
+        try {
+            $job->update($request->all());
+            $res = ApiHelper::createApiResponse(false, 200, 'Job updated successfully', null);
+            return response()->json($res, 200);
+        }catch (Exception $exception){
+            $res = ApiHelper::createApiResponse(true, 400, 'Job update failed', null);
+            return response()->json($res, 400);
+        }
+
     }
 
     public function destroy($id){
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('jobs', 'JobController')->except('create', 'edit');
+Route::apiResource('jobs', 'JobController');
