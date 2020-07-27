8da39dba9d6533a0342cf3596afca10a57b8686b
Sherif Tarek <sherift1552@gmail.com>
2020-07-27 18:02:27 +0200

    add applicant scope to the applicant model to retrive only the users with the role 'applicant'

--- app/Applicant.php
+++ app/Applicant.php
@@ -2,7 +2,17 @@
 
 namespace App;
 
+use App\Scopes\ApplicantScope;
+
 class Applicant extends User
 {
-    //
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope(new ApplicantScope);
+    }
 }
