27715ca4b390747a19e739c7397824c8f9d0f6a5
Sherif Tarek <sherift1552@gmail.com>
2020-07-27 17:59:30 +0200

    add recruiter scope to the recruiter model to retrive only the users with the role 'recruiter'

--- app/Recruiter.php
+++ app/Recruiter.php
@@ -2,10 +2,21 @@
 
 namespace App;
 
+use App\Scopes\RecruiterScope;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Recruiter extends User
-{
+{	
+	/**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope(new RecruiterScope);
+    }
+
     public function jobs(): HasMany
     {
         return $this->hasMany(Job::class);
