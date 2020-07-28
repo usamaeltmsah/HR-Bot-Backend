72150c53862ecbda46d0079c552e2fe099765813
Sherif Tarek <sherift1552@gmail.com>
2020-07-28 17:38:26 +0200

    develop the logic of updating a specific skill

--- app/Http/Controllers/SkillsController.php
+++ app/Http/Controllers/SkillsController.php
@@ -23,46 +23,19 @@ class SkillsController extends Controller{
           return new SkillResource($skill);
     }
 
-//    /**
-//     * Store a newly created resource in storage.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @return SkillResource
-//     */
-//    public function store(Request $request):SkillResource
-//    {
-//        $request->validate([
-//           'name' => 'required',
-//        ]);
-//
-//        $skill =Skill::create($request->all());
-//        return new SkillResource($skill);
-//    }
+   /**
+    * Update the specified skill in storage.
+    *
+    * @param  \App\Http\Requests\SkillFormRequest $request
+    * @param  \App\Skill $skill
+    * @return \App\Http\Resources\SkillResource
+    */
+   public function update(SkillFormRequest $request, Skill $skill): SkillResource
+   {
+       $skill->update($request->validated());
 
-//    /**
-//     * Display the specified resource.
-//     *
-//     * @param  \App\Skill $skill
-//     * @return SkillResource|\Illuminate\Http\Response
-//     */
-//    public function show(Skill $skill):SkillResource
-//    {
-//        return new SkillResource(Skill::firstWhere('id', $skill->id));
-//    }
-
-//    /**
-//     * Update the specified resource in storage.
-//     *
-//     * @param  \Illuminate\Http\Request $request
-//     * @param  \App\Skill $skill
-//     * @return SkillResource|\Illuminate\Http\Response
-//     */
-//    public function update(Request $request, Skill $skill): SkillResource
-//    {
-//        $skill->update($request->all());
-//
-//        return new SkillResource($skill);
-//    }
+       return new SkillResource($skill);
+   }
 
 //    /**
 //     * Remove the specified resource from storage.
@@ -73,11 +46,11 @@ class SkillsController extends Controller{
 //    public function destroy(Skill $skill)
 //    {
 //        $skill->delete();
-//
-////        $response = [
-////            'msg' => 'Skill'.$skill.'successfully deleted',
-////        ];
-////        return response()->json($response, 204);
+
+// //        $response = [
+// //            'msg' => 'Skill'.$skill.'successfully deleted',
+// //        ];
+// //        return response()->json($response, 204);
 //        return response()->json();
 //    }
 }
