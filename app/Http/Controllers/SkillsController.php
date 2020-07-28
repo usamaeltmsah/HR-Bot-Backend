20fa222e58fba743c8bb04c5f666f49ec6a5b8ab
Sherif Tarek <sherift1552@gmail.com>
2020-07-28 17:52:14 +0200

    add type binding and comments to skills controller

--- app/Http/Controllers/SkillsController.php
+++ app/Http/Controllers/SkillsController.php
@@ -3,24 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Skill;
-use App\Http\Requests\SkillFormRequest;
+use Illuminate\Http\Response;
 use App\Http\Resources\SkillResource;
+use App\Http\Requests\SkillFormRequest;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class SkillsController extends Controller{
+class SkillsController extends Controller {
 
-    public function index(){
+    /**
+     * Get all the skills
+     * 
+     * @return \Illuminate\Http\Resources\Json\ResourceCollection
+     */
+    public function index(): ResourceCollection {
         $skills = Skill::orderBy('id', 'desc')->paginate();
         return SkillResource::collection($skills);
     }
 
-    public function show(Skill $skill){
+    /**
+     * Get the skill with the given id
+     * 
+     * @param  \App\Skill $skill
+     * @return \App\Http\Resources\SkillResource
+     */
+    public function show(Skill $skill): SkillResource {
         return new SkillResource($skill);
     }
 
-    public function store(SkillFormRequest $request){
-
-          $skill = Skill::create($request->validated());
-          return new SkillResource($skill);
+    /**
+     * Store a new skill with the given data
+     * 
+     * @param  \App\Http\Requests\SkillFormRequest $request
+     * @return \App\Http\Resources\SkillResource
+     */
+    public function store(SkillFormRequest $request): SkillResource {
+        $skill = Skill::create($request->validated());
+        return new SkillResource($skill);
     }
 
    /**
@@ -30,8 +48,7 @@ class SkillsController extends Controller{
     * @param  \App\Skill $skill
     * @return \App\Http\Resources\SkillResource
     */
-   public function update(SkillFormRequest $request, Skill $skill): SkillResource
-   {
+   public function update(SkillFormRequest $request, Skill $skill): SkillResource {
        $skill->update($request->validated());
 
        return new SkillResource($skill);
@@ -43,8 +60,7 @@ class SkillsController extends Controller{
     * @param  \App\Skill  $skill
     * @return \Illuminate\Http\Response
     */
-   public function destroy(Skill $skill)
-   {
+   public function destroy(Skill $skill): Response {
        $skill->delete();
        return response(null, 204);
    }
