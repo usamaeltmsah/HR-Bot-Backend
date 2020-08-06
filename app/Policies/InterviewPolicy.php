e201308ea3407b2866f29ef9735b22fecf0f833a
Sherif Tarek <sherift1552@gmail.com>
2020-08-07 01:03:36 +0200

    move interview questions authorization to the interview policy

---
+++ app/Policies/InterviewPolicy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Interview;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class InterviewPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Check wether the current user can retrive the questions for the given job
+     * @param  App\User   $user 
+     * @param  App\Interview    $interview  
+     * @return boolean
+     */
+    public function retriveQuestions(User $user, Interview $interview): bool
+    {
+
+        if($user->isApplicant() && $interview->applicant_id == $user->getKey()) {
+            return $interview->isInProgress();
+        }
+
+        return False;
+    }
+}
--- app/Policies/JobPolicy.php
+++ app/Policies/JobPolicy.php
@@ -11,27 +11,6 @@ class JobPolicy
     use HandlesAuthorization;
 
     /**
-     * Check wether the current user can retrive the questions for the given job
-     * @param  App\User   $user 
-     * @param  App\Job    $job  
-     * @return boolean
-     */
-    public function retriveQuestions(User $user, Job $job): bool
-    {
-        if($user->isRecruiter()) {
-        	return $job->isCreatedBy($user);
-        }
-
-        if($user->isApplicant()) {
-            if($interview = $job->getInterviewFor($user)) {
-                return $interview->isInProgress();
-            }
-        }
-
-        return False;
-    }
-
-    /**
      * Check wether the current user can apply on the given job
      * @param  App\User   $user 
      * @param  App\Job    $job  
