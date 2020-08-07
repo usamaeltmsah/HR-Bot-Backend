b46dff896a0fe8de38b6402e908ba9ceb6966c77
Sherif Tarek <sherift1552@gmail.com>
2020-08-07 18:42:39 +0200

    add is accepting interviews utility method to the job model

--- app/Job.php
+++ app/Job.php
@@ -39,6 +39,15 @@ class Job extends Model
     }
     
     /**
+     * Check whether this job is accepting interviews
+     * @return boolean
+     */
+    public function isAcceptingInteviews(): bool
+    {
+        return now()->between($this->accept_interviews_from, $this->accept_interviews_until);
+    }
+
+    /**
      * Check wether this job has interview for the given user
      * 
      * @param  \App\User    $user
