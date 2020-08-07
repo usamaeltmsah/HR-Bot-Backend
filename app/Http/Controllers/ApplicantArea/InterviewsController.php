423dcc064737cd36521837036a17b4d39f2dc954
Sherif Tarek <sherift1552@gmail.com>
2020-08-07 14:59:41 +0200

    use the applicant area question jos resource

--- app/Http/Controllers/ApplicantArea/InterviewsController.php
+++ app/Http/Controllers/ApplicantArea/InterviewsController.php
@@ -6,8 +6,7 @@ use App\Question;
 use App\Interview;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\QuestionResource;
-use App\Http\Requests\InterviewAnswerFormRequest;
+use App\Http\Resources\ApplicantArea\QuestionResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
@@ -22,23 +21,5 @@ class InterviewsController extends Controller
      */
     public function questions(Request $request, Interview $interview) : ResourceCollection {
         return QuestionResource::collection($interview->questions);
-    }
-
-	/**
-     * Get all questions for the given jobs
-     * 
-     * @param \App\Interview $interview
-     * 
-     * @return \Illuminate\Http\Resources\Json\ResourceCollection
-     */
-    public function answer(InterviewAnswerFormRequest $request, Interview $interview, Question $question) : ResourceCollection {
-
-    	$data = $request->validated();
-    	
-    	$data['question_id'] = $question->getKey();
-
-    	$answer = $interview->answers()->create($data);
-
-        return AnswerResource::collection($answer);
     }
 }
