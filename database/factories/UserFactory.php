b0222c518656026b507625ae34a4ec1d64163204
usamaeltmsah <usamaeltmsah@gmail.com>
2020-08-07 02:04:16 +0200

    Test that applicant can get job questions

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,13 +3,32 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
+use App\Applicant;
 use Faker\Generator as Faker;
 
-$factory->define(User::class, function (Faker $faker) use($x){
+$factory->define(User::class, function (Faker $faker){
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => \Hash::make('12345678'),
         'role' => $faker->randomNumber() % 2 == 0 ? 'recruiter' : 'applicant',
+    ];
+});
+
+$factory->define(Applicant::class, function (Faker $faker){
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => \Hash::make('12345678'),
+        'role' => 'applicant',
+    ];
+});
+
+$factory->define(Recruiter::class, function (Faker $faker){
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => \Hash::make('12345678'),
+        'role' => 'recruiter',
     ];
 });
--- tests/Feature/Http/Controllers/Api/JobQuestionsControllerTest.php
+++ tests/Feature/Http/Controllers/Api/JobQuestionsControllerTest.php
@@ -5,9 +5,15 @@ namespace Tests\Feature\Http\Controllers\Api;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Applicant;
+use App\Interview;
+use App\Job;
+use Laravel\Passport\Passport;
 
 class JobQuestionsControllerTest extends TestCase
 {
+    //use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
@@ -15,11 +21,23 @@ class JobQuestionsControllerTest extends TestCase
      */
     public function test_guest_cant_get_job_questions()
     {
-        $response = $this->json('GET', '/api/applicant/jobs/2/questions', [
-            
-        ]);
+        $response = $this->json('GET', '/api/applicant/jobs/2/questions');
 
         $response->assertStatus(401);
 
     }
+
+    public function test_applicant_can_get_job_questions()
+    {
+
+        $user = factory(Applicant::class)->create();
+        $job = factory(Job::class)->create();
+        // $interview = Interview::create(['applicant_id' => $user->getKey()]);
+        $interview = $job->interviews()->create(['applicant_id' => $user->getKey() ]);
+        $response = Passport::actingAs($user);
+        $response = $this->json('GET', route('applicantarea.jobs.apply', ['job' => $job->getRouteKey()]));
+
+        $response->assertStatus(200);
+    }
+    
 }
