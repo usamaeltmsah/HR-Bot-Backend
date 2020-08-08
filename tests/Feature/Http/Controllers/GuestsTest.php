c8cd773124a29efda4e99be56b645b62b79599f2
usamaeltmsah <usamaeltmsah@gmail.com>
2020-08-08 19:25:40 +0200

    Test that any guest can register as an applicant with non-existing email

---
+++ tests/Feature/Http/Controllers/GuestsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+use App\User;
+
+
+class GuestsTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_guest_can_register_as_applicant()
+    {
+        $user = factory('App\Applicant')->raw();
+
+        $response = $this->post('api/register', $user);
+        
+        $response->assertStatus(201);
+    }
+}
