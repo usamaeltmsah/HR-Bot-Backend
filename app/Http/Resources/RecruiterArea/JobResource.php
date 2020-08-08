6022df6bf305faf4ee1126ebf00e3f6145306da3
Sherif Tarek <sherift1552@gmail.com>
2020-08-08 03:36:10 +0200

    develop the job resource in recruiter area

---
+++ app/Http/Resources/RecruiterArea/JobResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\RecruiterArea;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class JobResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $job = $this->resource;
+
+        return [
+            'id' => (string) $job->getRouteKey(),
+            'recruiter_id' => (string) $job->recruiter_id,
+            'title' => (string) $job->title,
+            'description' => (string) $job->description,
+            'accept_interviews_from' => (string) $job->accept_interviews_from,
+            'accept_interviews_until' => (string) $job->accept_interviews_until,
+            'interview_duration' => (string) $job->interview_duration,
+            'created_at' => (string) $job->created_at,
+            'updated_at' => (string) $job->updated_at,
+        ];
+    }
+}
