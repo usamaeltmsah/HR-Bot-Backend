96eb4f34458114eba0a029bca998a7cedbf5671b
usamaeltmsah <usamaeltmsah@gmail.com>
2020-08-09 20:11:22 +0200

    Change the format of date

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -9,8 +9,8 @@ $factory->define(Job::class, function (Faker $faker) {
     return [
         'title' => $faker->name,
         'description' => $faker->sentences(4, True),
-        'accept_interviews_from' => now(),
-        'accept_interviews_until'=> $faker->dateTimeBetween('now', '+30 years'),
+        'accept_interviews_from' => now()->format('Y-m-d H:i:s'),
+        'accept_interviews_until'=> $faker->dateTimeBetween('now', '+30 years')->format('Y-m-d H:i:s'),
         'interview_duration' => $faker->randomNumber(4),
         'recruiter_id' => 1
     ];
