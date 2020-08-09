e5b8bbb3d1e6ed33237140d46aa7c7ba8c86ae29
Sherif Tarek <sherift1552@gmail.com>
2020-08-09 03:00:02 +0200

    retrive all the jobs data for the applicant and the guest

--- app/Http/Resources/ApplicantArea/JobResource.php
+++ app/Http/Resources/ApplicantArea/JobResource.php
@@ -20,6 +20,10 @@ class JobResource extends JsonResource
             'id' => (string) $job->getRouteKey(),
             'title' => (string) $job->title,
             'description' => (string) $job->description,
+            'accept_interviews_from' => (string) $job->accept_interviews_from,
+            'accept_interviews_until' => (string) $job->accept_interviews_until,
+            'interviews_duration' => (string) $job->interviews_duration,
+            'recruiter_id' => (string) $job->recruiter_id,
         ];
     }
 }
