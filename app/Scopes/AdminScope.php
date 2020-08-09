f8d39e7fc09d21c42a7e5542b2030a8ae97809e0
Sherif Tarek <sherift1552@gmail.com>
2020-08-10 00:41:10 +0200

    fix admin scope name typo

--- app/Scopes/AdminScope.php
+++ app/Scopes/AdminScope.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 
-class RecruiterScope implements Scope
+class AdminScope implements Scope
 {
     /**
      * Apply the scope to a given Eloquent query builder.
