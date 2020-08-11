e7795de33b0823ceb1a10b2ef1f537dc1c0c68f5
usamaeltmsah <usamaeltmsah@gmail.com>
2020-08-11 04:45:12 +0200

    Test that admin can add model answer for a question

--- tests/Feature/Http/Controllers/Api/ModelAnswersTest.php
+++ tests/Feature/Http/Controllers/Api/ModelAnswersTest.php
@@ -8,14 +8,11 @@ use Tests\TestCase;
 use App\Admin;
 use App\Skill;
 use App\Question;
-use App\Answer;
 use Laravel\Passport\Passport;
 
 class ModelAnswersTest extends TestCase
 {
     private $admin = null;
-    private $skill = null;
-    private $answer = null;
     private $question = null;
 
     public function setUp(): void
@@ -23,7 +20,6 @@ class ModelAnswersTest extends TestCase
         parent::setUp();
 
         $this->admin = factory(Admin::class)->create();
-        $this->skill = factory(Skill::class)->create();
         $this->question = factory(Question::class)->create();
     }
     /**
@@ -50,5 +46,28 @@ class ModelAnswersTest extends TestCase
             ]
         ];
         $response->assertJsonStructure($structure)->assertStatus(200);
+    }
+
+    public function test_admin_can_add_modelAnswer_for_existing_question()
+    {
+        Passport::actingAs($this->admin, [], 'admin');
+        
+        $question = factory(Question::class)->create();
+        $question->modelAnswers()->create(['body' => "Answer # 1"]);
+
+        $url = route('adminarea.questions.model_answers.store', ['question' => $question->getRouteKey()]);
+        
+        $response = $this->post($url, ['body' => "NEW ANSWER", "question_id" => $question->getRouteKey()]);
+
+        // Count the number of model answers of this question
+        $n_modelAnswers = count($question->modelAnswers);
+
+        // The added model answer will be always the last model answer so need to check that it added successfully
+        $added_modelAnswers = [$question->modelAnswers[$n_modelAnswers-1]['id'], $question->modelAnswers[$n_modelAnswers-1]['body']];
+        $added_modelAnswers_from_response = [$response["data"]["id"], $response["data"]["body"]];
+
+        // Test that the 2 arrays are equals 
+        $this->assertEquals($added_modelAnswers, $added_modelAnswers_from_response);
+        $response->assertStatus(201);
     }
 }
