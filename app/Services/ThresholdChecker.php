e15231051651b3586ed6a7d131baccafc47e0a48
Sherif Tarek <sherift1552@gmail.com>
2020-08-15 04:47:26 +0200

    make threshold checker accepts the threshold as a parameter

--- app/Services/ThresholdChecker.php
+++ app/Services/ThresholdChecker.php
@@ -7,12 +7,28 @@ use Illuminate\Support\Facades\Http;
 class ThresholdChecker
 {
 	/**
+	 * threshold to compare the values to
+	 * 
+	 * @var float
+	 */
+	protected $threshold;
+
+	public function __construct(?float $threshold = null)
+	{
+		if (is_null($threshold)) {
+			$this->threshold = config('hrbot.threshold');
+		} else {
+			$this->threshold = $threshold;
+		}
+	}
+
+	/**
 	 * Evaluate the given answer compared to the given model answers
 	 *
 	 * @return boolean
 	 */
 	public function check(string $value): bool
 	{
-		return $value < config('hrbot.threshold');
+		return $value < $this->threshold;
 	}
 }
